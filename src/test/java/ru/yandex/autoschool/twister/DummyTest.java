0fc645b689ae9859d76f666abfd19c1f7f7f48e5
Julia <q@Julias-MacBook-Air-2.local>
2014-11-18 22:57:00 +0400

    Test directory and dummy test

---
+++ src/test/java/ru/yandex/autoschool/twister/DummyTest.java
@@ -0,0 +1,15 @@
+package ru.yandex.autoschool.twister;
+
+import org.junit.Test;
+
+/**
+ * Created by q on 11/18/14.
+ */
+public class DummyTest {
+
+    @Test
+    public void test() {
+
+
+    }
+}
