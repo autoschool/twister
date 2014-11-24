d8b8c72b258ded5f3458c020fd48e4b935b77e33
Anastasiya Shchogoleva <ashchogoleva@gmail.com>
2014-11-25 02:23:07 +0300

    Add AuthUser context

--- src/main/java/ru/qatools/school/twister/models/User.java
+++ src/main/java/ru/qatools/school/twister/models/User.java
@@ -2,28 +2,29 @@ package ru.qatools.school.twister.models;
 
 import org.javalite.activejdbc.Model;
 
+import java.security.Principal;
 import java.sql.Timestamp;
 
 /*
  * Created by ashchogoleva on 23.11.14.
  */
-public class User extends Model{
+public class User extends Model implements Principal {
 
-  public String getName(){
-      return getString("name");
-  }
+    public String getName() {
+        return getString("name");
+    }
 
-  public void setName(String name){
-      setString("name",name);
-  }
+    public void setName(String name) {
+        setString("name", name);
+    }
 
-  public String getPassHash(){
-      return getString("pass_hash");
-  }
+    public String getPassHash() {
+        return getString("pass_hash");
+    }
 
-  public void setPassHash(String hash){
-      setString("pass_hash", hash);
-  }
+    public void setPassHash(String hash) {
+        setString("pass_hash", hash);
+    }
 
     public Timestamp getCreatedAt() {
         return getTimestamp("created_at");
